d723a0bfc0da5ef7a50630ff3bb23a17cbddeb23
InJung Chung <mu29gl@gmail.com>
2015-10-17 02:12:52 +0900

    add get cookie function

--- Crawler.py
+++ Crawler.py
@@ -1,14 +1,31 @@
 #-*- coding: utf-8 -*-
 
 import urllib
+import urllib2
+from settings import *
 from bs4 import BeautifulSoup
-from Result import *
 
 class Crawler:
     def __init__(self):
         self.base_url = 'http://tutor.orbi.kr/teacher/'
+        self.cookie = COOKIE
 
     def get_url(self, no):
         return self.base_url + str(no) + '/bookmark'
 
+    def get_cookie(self):
+        url = 'https://login.orbi.kr/user/login/tutor'
+        login_query = urllib.urlencode(USER_INFO)
+        request = urllib2.Request(url,login_query)
+        response = urllib2.urlopen(request)
+        cookie = response.headers.get('Set-Cookie')
+        return cookie
+
     def get_profile(self, no):
+        url = self.get_url(no)
+        request = urllib2.Request(url)
+        request.add_header('cookie', self.cookie)
+        response = urllib2.urlopen(request)
+
+        print response.read()
+        soup = BeautifulSoup(response.read(), 'html.parser')
