8a46edb7d18ac98295c2147fa7f8f2d38e334e57
InJung Chung <mu29gl@gmail.com>
2015-10-17 02:42:02 +0900

    add profile model

---
+++ profile.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+
+class Profile:
+    def __init__(self, name, subject, age, school, phone):
+        self.name = name
+        self.subject = subject
+        self.age = age
+        self.school = school
+        self.phone = phone
