f1cd21f65e0bc4809146b4fdec294c5aa854479b
InJung Chung <mu29gl@gmail.com>
2015-10-17 21:53:24 +0900

    main logic

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 #-*- coding: utf-8 -*-
 
-import urllib
+import time
 from crawler import *
+from database import *
 
 crawler = Crawler()
-crawler.get_profile(4)
+db = DataBase()
+
+while True:
+    try:
+        old = db.get_count() + 1
+        latest = crawler.get_latest_no() + 1
+
+        for i in range(old, latest):
+            profile = crawler.get_profile(i)
+            if not profile is None:
+                db.put(profile)
+
+        time.sleep(3600)
+    except:
+        for e in sys.exc_info
+            print e
+        continue
