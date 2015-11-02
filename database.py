99dd2e54040e736cd65f3c1952626a78d85b2845
InJung Chung <mu29gl@gmail.com>
2015-11-02 14:18:36 +0900

    avoid duplication

--- database.py
+++ database.py
@@ -14,9 +14,19 @@ class DataBase():
         return cursor
 
     def put(self, profile):
+        if self.check_exists(profile.phone):
+            return
+
         variables = "('{0}', '{1}', '{2}', '{3}', '{4}');".format(profile.name, profile.subject, profile.age, profile.school, profile.phone)
         query = "INSERT INTO `teachers`(`name`, `subject`, `age`, `school`, `phone`) VALUES " + variables
         self.execute(query).close()
+
+    def check_exists(self, phone):
+        query = "SELECT COUNT(*) AS count FROM `teachers` WHERE `phone` = '{0}';".format(phone)
+        cursor = self.execute(query)
+        result = cursor.fetchone()['count']
+        cursor.close()
+        return int(result) > 0
 
     def get_count(self):
         query = "SELECT COUNT(*) AS count FROM `teachers`;"
