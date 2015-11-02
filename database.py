96badd1402de36dcfb20edd5d2f87c4af7c41658
InJung Chung <mu29gl@gmail.com>
2015-11-02 17:39:48 +0900

    get teacher info instead of phone number

--- database.py
+++ database.py
@@ -2,8 +2,7 @@
 
 import MySQLdb
 
-class DataBase():
-
+class DataBase:
     def __init__(self):
         self.connection = MySQLdb.connect('localhost', 'root', 'projectDanbi', 'orbi_message', charset='utf8', use_unicode=True)
         self.connection.autocommit(True)
@@ -16,15 +15,14 @@ class DataBase():
     def put(self, profile):
         if self.is_exists(profile.phone):
             return
-
         variables = "('{0}', '{1}', '{2}', '{3}', '{4}');".format(profile.name, profile.subject, profile.age, profile.school, profile.phone)
         query = "INSERT INTO `teachers`(`name`, `subject`, `age`, `school`, `phone`) VALUES " + variables
         self.execute(query).close()
 
-    def get_phone(self, id):
-        query = "SELECT `phone` FROM `teachers` WHERE `id` = '{0}';".format(id)
+    def get_teacher(self, id):
+        query = "SELECT * FROM `teachers` WHERE `id` = '{0}';".format(id)
         cursor = self.execute(query)
-        result = cursor.fetchone()['phone']
+        result = cursor.fetchone()
         cursor.close()
         return result
 
